59dd0beacb1381b4e8c55e7f6e38f53e786123e8
wagolemusa <homiemusa@gmail.com>
2024-10-12 17:27:34 +0300

    final debug

--- next.config.mjs
+++ next.config.mjs
@@ -15,8 +15,8 @@
 
 const nextConfig = {
     env: {
-        ENVIRONMENT_URL: "https://godisgreat-f799547c5083.herokuapp.com",
-        NEXTAUTH_URL: "https://godisgreat-f799547c5083.herokuapp.com",
+        ENVIRONMENT_URL: "http://godisgreatgas.com",
+        NEXTAUTH_URL: "http://godisgreatgas.com",
         DB_URL: "mongodb+srv://homiemusa:djrefuge@cluster0.fk517ja.mongodb.net/npcdatabase?retryWrites=true&w=majority", 
         CLOUND_NAME:"xycoders",
         CLOUDINARY_API_KEY: "837887428819121",
